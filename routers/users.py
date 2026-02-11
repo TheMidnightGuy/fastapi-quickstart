26d186a7b1e2d0a97f41a8463b6cc2cc99ffde34
TheMidnightGuy <themidnightguyyy@gmail.com>
2026-02-11 11:10:43 -0300

    first commit

---
+++ routers/users.py
@@ -0,0 +1,15 @@
+#Obtenemos resultados en http://127.0.0.1:8000/users/me
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
